ee606cf389821a6e9a4d2e93513c4996003a9a20
Freyr H <freyr.helga@gmail.com>
2023-11-26 23:35:06 +0000

    Add gh Copilot CLI setup script for Dev Containers

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -10,36 +10,8 @@ NC='\033[0m' # No Color
 # Set Root as the owner of the current working directory
 sudo chown -R root:root .
 
-# Check if we have a .config/gh/config.yml file and if it contains a token
-if [ -f ~/.config/gh/config.yml ] || grep -q token ~/.config/gh/config.yml; then
-    echo "Found GitHub CLI token in ~/.config/gh/config.yml"
-    echo "Installing GitHub Copilot extension for CLI"
-    gh extension install github/gh-copilot    
-
-    # Add permanent aliases
-    echo -e "${BLUE}Adding permanent aliases for GitHub Copilot${NC}"
-    echo -e "Setting alias ${GREEN}copilot${NC} for ${GREEN}gh copilot${NC}"
-    alias copilot="gh copilot"; echo "alias copilot='gh copilot'" >> /etc/bash.bashrc
-    echo -e "Setting alias ${GREEN}?sh${NC} for ${GREEN}gh copilot suggest -t shell${NC}"
-    alias ?sh="gh copilot suggest -t shell"; echo "alias ?sh='gh copilot suggest -t shell'" >> /etc/bash.bashrc
-    echo -e "Setting alias ${GREEN}?gh${NC} for ${GREEN}gh copilot suggest -t gh${NC}"
-    alias ?gh="gh copilot suggest -t gh"; echo "alias ?gh='gh copilot suggest -t gh'" >> /etc/bash.bashrc
-    echo -e "Setting alias ${GREEN}?git${NC} for ${GREEN}gh copilot suggest -t git${NC}"
-    alias ?git="gh copilot suggest -t git"; echo "alias ?git='gh copilot suggest -t git'" >> /etc/bash.bashrc
-
-    echo -e "Setting alias ${GREEN}??sh${NC} for ${GREEN}gh copilot explain -t shell${NC}"
-    alias ??sh="gh copilot explain -t shell"; echo "alias ??sh='gh copilot explain -t shell'" >> /etc/bash.bashrc
-    echo -e "Setting alias ${GREEN}??gh${NC} for ${GREEN}gh copilot explain -t gh${NC}"
-    alias ??gh="gh copilot explain -t gh"; echo "alias ??gh='gh copilot explain -t gh'" >> /etc/bash.bashrc
-    echo -e "Setting alias ${GREEN}??git${NC} for ${GREEN}gh copilot explain -t git${NC}"
-    alias ??git="gh copilot explain -t git"; echo "alias ??git='gh copilot explain -t git'" >> /etc/bash.bashrc
-else
-    echo "No GitHub CLI token found in ~/.config/gh/config.yml"
-    echo "Skipping installation of GitHub Copilot extension for CLI"
-    echo "If you want to use GitHub Copilot, please run 'gh auth login' to login to GitHub CLI"
-    echo "Then run 'gh extension install github/gh-copilot' to install the GitHub Copilot extension for CLI"
-fi
-
+# Include the gh_copilot_cli_on_dev_container.sh script to install the GitHub Copilot CLI extension for GitHub CLI
+. $REPO_ROOT/gh_copilot_cli_on_dev_container.sh
 
 echo -e "${GREEN}Done!${NC}"
 echo -e "${YELLOW}If the alias ?? is not working, please restart your terminal session${NC}"
--- README.md
+++ README.md
@@ -22,4 +22,23 @@ Options:
 Example: img_opt.sh (asks for width and quality, optimizes all images in the current directory but doesn't crawl through subdirectories)  
 Example: img_opt.sh -Rw 1920 -q 60 -t jpg png -d ./static/img (optimizes all jpg and png images in ./static/img/ and its subdirectories with a maximum width of 1920px and a quality of 60%)  
 Example: img_opt.sh -r (restores all images in the current directory)  
-Example: img_opt.sh -Rrd ./img (recursively restores all images in the "img" subdirectory and its subfolders)  
+Example: img_opt.sh -Rrd ./img (recursively restores all images in the "img" subdirectory and its subfolders)
+
+## gh_copilot_cli_on_dev_container.sh - Drop-in script for enabling GitHub Copilot CLI on a dev container
+Include the script in your devcontainer.json file like this:
+```
+"postCreateCommand": "bash -c 'chmod +x ${localWorkspaceFolder}/.devcontainer/gh_copilot_cli_on_dev_container.sh && ${localWorkspaceFolder}/.devcontainer/gh_copilot_cli_on_dev_container.sh'",
+```
+You can also use it as a drop-in script to be included in other scripts, however you decide to do that.
+
+Requires:
+ - GitHub CLI must be installed on the container (this is done in the devcontainer.json file or Dockerfile or similar)
+ - GitHub CLI must be configured with a token in ~/.config/gh/config.yml on the container
+  - This can be done by adding the following to your devcontainer.json file:
+  ```
+  "source=${localEnv:USERPROFILE}\\.config\\gh,target=/root/.config/gh,type=bind,consistency=cached"
+  ```
+  Where ${localEnv:USERPROFILE} is the path to your user profile on the host machine. This is usually C:\Users\yourusername on Windows, and /home/yourusername on Linux and Mac.
+
+
+
